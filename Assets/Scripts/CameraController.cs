4733e6fc7ea45eb2ea3beefbf55df03e94a6092c
Kamal Ranaweera <kamal.ranaweera@ualberta.ca>
2016-08-25 16:38:19 -0600

    Initial Import of Roll a Ball project. All game objects of this version a have been placed manually.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	
+	}
+	
+	// Update is called once per frame, after all components have been positioned.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;	
+	}
+		
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+	private Vector3 force;
+
+	private void setCount()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 11)
+			winText.text = "Game Complete!";
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCount ();
+		winText.text = "";
+		force = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		force = movement * speed;
+		rb.AddForce (force);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			++count;
+			setCount ();
+		}
+	}
+
+	public void Reset()
+	{
+		transform.position = new Vector3(0, 0.5f, 0);
+		rb.velocity = Vector3.zero;
+		rb.AddForce (-force);
+		force = Vector3.zero;
+	}
+
+	public void Export()
+	{
+		List<Vector3> vert_list = new List<Vector3>();
+
+		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject> ();
+		foreach (GameObject go in allObjects) {
+			if (go.activeInHierarchy) {
+				if (go.CompareTag("Export")) {
+					MeshFilter mf = go.GetComponent<MeshFilter> ();
+					Vector3[] veticies = mf.mesh.vertices;
+					vert_list.AddRange (veticies);
+
+					print (go.name +": position = " + go.transform.position);
+					print (go.name +": rotation = " + go.transform.rotation);
+					print (go.name +": localScale = " + go.transform.localScale);
+					print ("verticies:");
+					int i = 0;
+					foreach (Vector3 v in veticies) {
+						print (++i + ": " + v);
+					}
+
+				}
+			}
+		}
+
+		List<string> str_list = new List<string> ();
+		foreach (Vector3 v in vert_list) {
+			str_list.Add(v.x + " " + v.y + " " + v.z);
+		}
+
+		string str = string.Join (";", str_list.ToArray());
+		print (str);
+	}
+}
